995c08ea338cc443c92873fbdb08439a7ce39777
Alex Kiesel <alex@kiesel.name>
2013-09-07 20:53:06 +0200

    - extract method: emitContent(), encode() - move to base class

--- core/src/main/php/xml/NodeEmitter.class.php
+++ core/src/main/php/xml/NodeEmitter.class.php
@@ -5,9 +5,9 @@
  *
  */
 
-  uses('xml.NodeEmitter');
+  abstract class NodeEmitter extends Object {
 
-  class WrappedIndentNodeEmitter extends NodeEmitter {
+    public abstract function emit(Node $node, $encoding, $inset= '');
 
     protected function encode($string, $encoding) {
       if (xp::ENCODING != $encoding) {
@@ -36,39 +36,6 @@
       }
     }
 
-    public function emit(Node $node, $encoding, $inset= '') {
-      $xml= $inset.'<'.$node->getName();
 
-      $content= $this->emitContent($node, $encoding);
-
-      if ($node->attribute) {
-        $sep= (sizeof($node->attribute) < 3) ? '' : "\n".$inset;
-        foreach ($node->attribute as $key => $value) {
-          $xml.= $sep.' '.$key.'="'.htmlspecialchars(
-            $this->encode($value, $encoding),
-            ENT_COMPAT,
-            xp::ENCODING
-          ).'"';
-        }
-        $xml.= $sep;
-      }
-
-      // No content and no children => close tag
-      if (0 == strlen($content)) {
-        if (!$node->children) return $xml."/>\n";
-        $xml.= '>';
-      } else {
-        $xml.= '>'."\n  ".$inset.$content;
-      }
-
-      if ($node->children) {
-        $xml.= "\n";
-        foreach ($node->children as $child) {
-          $xml.= $this->emit($child, $encoding, $inset.'  ');
-        }
-        $xml= substr($xml, 0, -1).$inset;
-      }
-      return $xml."\n".$inset.'</'.$node->name.">\n";
-    }
   }
 ?>
