a525a0eee23b1a38211e6b3ca3ab6c922821e3b7
Alex Kiesel <alex@kiesel.name>
2013-09-07 20:53:34 +0200

    - cleanup leftover code

--- core/src/main/php/xml/WrappedIndentNodeEmitter.class.php
+++ core/src/main/php/xml/WrappedIndentNodeEmitter.class.php
@@ -9,33 +9,6 @@
 
   class WrappedIndentNodeEmitter extends NodeEmitter {
 
-    protected function encode($string, $encoding) {
-      if (xp::ENCODING != $encoding) {
-        return iconv(xp::ENCODING, $encoding, $string);
-      }
-
-      return $string;
-    }
-
-    protected function emitContent(Node $node, $encoding) {
-      if ('string' == ($type= gettype($node->content))) {
-        return $this->encode(htmlspecialchars($node->content, ENT_COMPAT, xp::ENCODING), $encoding);
-      } else if ('float' == $type) {
-        return ($node->content - floor($node->content) == 0)
-          ? number_format($node->content, 0, NULL, NULL)
-          : $node->content
-        ;
-      } else if ($node->content instanceof PCData) {
-        return $this->encode($node->content->pcdata, $encoding);
-      } else if ($node->content instanceof CData) {
-        return '<![CDATA['.str_replace(']]>', ']]]]><![CDATA[>', $this->encode($node->content->cdata, $encoding)).']]>';
-      } else if ($node->content instanceof String) {
-        return htmlspecialchars($node->content->getBytes($encoding), ENT_COMPAT, $encoding);
-      } else {
-        return $node->content;
-      }
-    }
-
     public function emit(Node $node, $encoding, $inset= '') {
       $xml= $inset.'<'.$node->getName();
 
