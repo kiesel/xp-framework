7e1bdfa7a6bf8262c999b1c7a418eb03f3c12a65
Alex Kiesel <alex@kiesel.name>
2013-09-07 20:57:01 +0200

    - apply refactoring to siblings

--- core/src/main/php/xml/NoIndentNodeEmitter.class.php
+++ core/src/main/php/xml/NoIndentNodeEmitter.class.php
@@ -5,40 +5,17 @@
  *
  */
 
-  class NoIndentNodeEmitter extends Object {
+  uses('xml.NodeEmitter');
+
+  class NoIndentNodeEmitter extends NodeEmitter {
     public function emit(Node $node, $encoding, $inset= '') {
       $xml= $inset.'<'.$node->getName();
-      $conv= xp::ENCODING != $encoding;
-      
-      if ('string' == ($type= gettype($node->content))) {
-        $content= $conv
-          ? iconv(xp::ENCODING, $encoding, htmlspecialchars($node->content, ENT_COMPAT, xp::ENCODING))
-          : htmlspecialchars($node->content, ENT_COMPAT, xp::ENCODING)
-        ;
-      } else if ('float' == $type) {
-        $content= ($node->content - floor($node->content) == 0)
-          ? number_format($node->content, 0, NULL, NULL)
-          : $node->content
-        ;
-      } else if ($node->content instanceof PCData) {
-        $content= $conv
-          ? iconv(xp::ENCODING, $encoding, $node->content->pcdata)
-          : $node->content->pcdata
-        ;
-      } else if ($node->content instanceof CData) {
-        $content= '<![CDATA['.str_replace(']]>', ']]]]><![CDATA[>', $conv
-          ? iconv(xp::ENCODING, $encoding, $node->content->cdata)
-          : $node->content->cdata
-        ).']]>';
-      } else if ($node->content instanceof String) {
-        $content= htmlspecialchars($node->content->getBytes($encoding), ENT_COMPAT, $encoding);
-      } else {
-        $content= $node->content; 
-      }
-      
+
+      $content= $this->emitContent($node, $encoding);
+
       foreach ($node->attribute as $key => $value) {
         $xml.= ' '.$key.'="'.htmlspecialchars(
-          $conv ? iconv(xp::ENCODING, $encoding, $value) : $value,
+          $this->encode($value, $encoding),
           ENT_COMPAT,
           xp::ENCODING
         ).'"';
